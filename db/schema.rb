cd0b87873d11ab5b32ec6850f87d1f2325a4582c
pascal <pascal.carrie@gmail.com>
2020-10-16 16:54:14 +0200

    confirmation mail on setup

--- db/schema.rb
+++ db/schema.rb
@@ -66,6 +66,10 @@ ActiveRecord::Schema.define(version: 2020_09_09_161541) do
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
+    t.string "confirmation_token"
+    t.datetime "confirmed_at"
+    t.datetime "confirmation_sent_at"
+    t.string "unconfirmed_email"
     t.boolean "is_admin", default: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
